4f5ded6ce05636486d51d604ca8c341ea92dc61f
Aliaksandr Pratasevich <urbankomp@gmail.com>
2023-08-24 16:03:38 +0300

    feat: implement task Towel Sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    if (!matrix) return [];
+    let newArr = matrix.reduce((accumulator, item, index) => {
+        return accumulator.concat((index % 2 === 0) ? item : item.reverse());
+    }, []);
+    return newArr;
 }
